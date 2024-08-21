5ce4fb2ad312fdfdc9334ef56b9e53ec6edb28d9
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-08-21 10:27:05 +0100

    untill class 134

---
+++ authors/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect, render
+from django.http import Http404
+
+from authors.forms import RegisterForm
+
+
+def register_view(request):
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    request.session['register_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
