8b104a6bf3c1b2ef333eb93e1bbe6aa0603cc75b
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-08-23 10:02:42 +0100

    untill class 152 - organizing files

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,91 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            "Must have at least one uppercase and one uppercase letter and one number"
+        ),
+            code='Invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your email')
+        add_placeholder(self.fields['first_name'], 'Ex: John')
+        add_placeholder(self.fields['last_name'], 'Ex: Doe')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
+
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        required=True,
+        label='First name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        required=True,
+        label='Last name'
+    )
+
+    username = forms.CharField(
+        error_messages={
+            'required': "This field can't be empty",
+            'min_lenght': "Username must have at least 4 and 150 characters",
+            'max_lenght': 'Username must have less than 150 characters'
+            },
+        required=True,
+        label='Username',
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        min_length=4,
+        max_length=150
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': "Email is not valid"},
+        required=True,
+        label='E-mail',
+        help_text='example@example.com'
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(),
+        error_messages={"required": "This field can't be empty"},
+        help_text="Must have at least one uppercase and one uppercase letter and one number",
+        validators=[strong_password],
+        label="Password"
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(),
+        error_messages={"required": "This field can't be empty"},
+        help_text="You need to repeat your password",
+        label="Password"
+    )
+
+
     class Meta:
         model = User
         fields = [
@@ -13,29 +96,29 @@ class RegisterForm(forms.ModelForm):
             'password'
             ]
         
-        labels = {
-            'username': "Username",
-            "first_name": "First name",
-            "last_name": "Last name",
-            "email": "E-mail",
-            "password": "Password"
-        }
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
 
-        help_texts = {
-            "email": "example@example.com"
-        }
+        if exists:
+            raise ValidationError('User e-mail is already in use', code='invalid',)
 
-        error_messages = {
-            "username": {
-                "required": "This field can't be empty",
-            }
-        }
+        return email
+    
+    def clean(self):
+        cleaned_data = super().clean()
 
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'placeholder': "Type your username"
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': "Type your password here"
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                    "The passwords don't match",
+                    code='Invalid'
+                )
+            raise ValidationError({
+                "password": password_confirmation_error,
+                "password2": [
+                    password_confirmation_error
+                ]
             })
-        }
