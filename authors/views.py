275f2478364dac6c994bb933bc484f0d98c44919
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-08-29 20:57:40 +0100

    aulas ate 184

--- authors/views.py
+++ authors/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm, LoginForm
 
+from recipes.models import Recipe
+
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -49,7 +51,6 @@ def login_create(request):
         raise Http404()
     
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -65,7 +66,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
     
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -81,3 +82,29 @@ def logout_view(request):
     messages.success(request, 'Logout successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    recipes = Recipe.objects.filter(
+        is_published = False,
+        author=request.user
+    )
+    return render(request, 'authors/pages/dashboard.html', context={
+        'recipes': recipes,
+    })
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.filter(
+        is_published = False,
+        author=request.user,
+        pk=id
+    )
+
+    if not recipe:
+        raise Http404
+    
+    return render(request, 'authors/pages/dashboard_recipe.html', context={
+        
+    })
