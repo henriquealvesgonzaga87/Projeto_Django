a6a010c935d86063d5db9a0d3e04dc7dac3d1af0
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-09-19 14:31:03 +0100

    finishes the section 20 reorganizing the settings, creating the tags and model validation

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,49 @@
 from django.http import Http404
-from django.db.models import Q
+from django.db.models import Q, F, Value
+from django.db.models.aggregates import Count
+from django.db.models.functions import Concat
 from django.http.response import HttpResponse as HttpResponse
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
 from recipes.models import Recipe
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 import os
 
 PER_PAGE = int(os.environ.get('PER_PAGE', default=6))
+
+
+def theory(request, *args, **kwargs):
+    # recipes = Recipe.objects.all()
+    # recipes = recipes.filter(title__icontains="Test")
+    # try:
+    #     recipes = Recipe.objects.get(pk=10000)
+    # except ObjectDoesNotExist:
+    #     recipes = None
+    recipes = Recipe.objects.all().annotate(
+        author_full_name=Concat(
+            F('author__first_name'), Value(' '),
+            F('author__last_name'), Value(' ('),
+            F('author__username'), Value(')'),
+        )
+    )
+    number_of_recipes = recipes.aggregate(number=Count('id'))
+
+    context = {
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number']
+    }
+    
+    return render(
+        request,
+        'recipes/pages/theory.html',
+        context=context
+    )
 
 
 class RecipeListViewBase(ListView):
@@ -27,6 +60,7 @@ class RecipeListViewBase(ListView):
         )
 
         queryset = queryset.select_related('author', 'category')
+        queryset = queryset.prefetch_related('tags')
         #queryset = queryset.prefetch_related('author', 'category') -> when it's many to many relationship
 
         return queryset
@@ -157,3 +191,27 @@ class RecipeDetailApi(RecipeDetail):
             recipe_dict,
             safe=False,
         )
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = "recipes/pages/tag.html"
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(tags__slug=self.kwargs.get("slug", ""))
+        return queryset
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(slug=self.kwargs.get("slug", "")).first()
+
+        if not page_title:
+            page_title = "No recipes found"
+
+        page_title = f"{page_title} - Tag |"
+
+        context.update({
+            'page_title': page_title,
+        })
+
+        return context
