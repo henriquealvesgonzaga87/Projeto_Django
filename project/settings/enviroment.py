3945e156042bcd1c5db58b76c489893402923976
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-03-23 20:36:38 +0000

    fixes settings

--- project/settings/enviroment.py
+++ project/settings/enviroment.py
@@ -16,9 +16,9 @@ DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = ['*']
 
-ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
-    get_env_variable('ALLOWED_HOSTS')
-)
+# ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
+#     get_env_variable('ALLOWED_HOSTS')
+# )
 CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
     get_env_variable('CSRF_TRUSTED_ORIGINS')
 )
