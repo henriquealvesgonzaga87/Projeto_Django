1b2a76fe024eb8e4d444415c39e29bbe615b9b68
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-03-25 23:13:55 +0000

    updates allowed hosts and csrf origins

--- project/settings/enviroment.py
+++ project/settings/enviroment.py
@@ -14,9 +14,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS: list[str] = ['*']
+ALLOWED_HOSTS: list[str] = ['www.recipesproject.hagpythondev.com']
 
-CSRF_TRUSTED_ORIGINS: list[str] = ['htpps://*']
+CSRF_TRUSTED_ORIGINS: list[str] = ['https://www.recipesproject.hagpythondev.com']
 
 # ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
 #     get_env_variable('ALLOWED_HOSTS')
