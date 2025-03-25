4e4c2dd5501aff9cbd70d48fbcde178d554e39f3
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-03-25 13:02:53 +0000

    correcting cors and csrf

--- project/settings/enviroment.py
+++ project/settings/enviroment.py
@@ -16,7 +16,7 @@ DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = ['*']
 
-CSRF_TRUSTED_ORIGINS: list[str] = ['*']
+CSRF_TRUSTED_ORIGINS: list[str] = ['htpps://*']
 
 # ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
 #     get_env_variable('ALLOWED_HOSTS')
